7fef81ea4274508b5aee5f62f676f68d262c9d34
Nofeet56 <egabemoore@yahoo.com>
2022-08-07 10:52:36 -0400

    ebic

--- example_mods/stages/bar.lua
+++ example_mods/stages/bar.lua
@@ -3,28 +3,28 @@ function onCreate()
 	scaleObject('stageback', 1.4, 1.4)
 
 	makeAnimatedLuaSprite('bartender', 'stages/bar/Boppers/Bartender', -150, 340);
-	addAnimationByPrefix('bartender', 'Bartender', 'BartenderBop instance', 24, true)
+	addAnimationByPrefix('bartender', 'Bartender', 'BartenderBop instance', 24, false);
 	scaleObject('bartender', 1.4, 1.4)
 
 	makeAnimatedLuaSprite('lad', 'stages/bar/Boppers/HeadAuxBop', -450, 390);
-	addAnimationByPrefix('lad', 'HeadAuxBop', 'HeadAuxBop', 24, true)
+	addAnimationByPrefix('lad', 'HeadAuxBop', 'HeadAuxBop', 24, false);
 	setScrollFactor('lad', 0.9, 0.9);
 	scaleObject('lad', 1.4, 1.4)
 
 	makeAnimatedLuaSprite('other', 'stages/bar/Boppers/LotazBop', 350, 390);
-	addAnimationByPrefix('other', 'LotazBop', 'LotazBop', 24, true)
+	addAnimationByPrefix('other', 'LotazBop', 'LotazBop', 24, false);
 	setScrollFactor('other', 0.9, 0.9);
 	scaleObject('other', 1.4, 1.4)
 	
 	makeAnimatedLuaSprite('guy', 'stages/bar/Boppers/TWTBop', 950, 390);
-	addAnimationByPrefix('guy', 'TWTBop', 'TWTBop', 24, true)
+	addAnimationByPrefix('guy', 'TWTBop', 'TWTBop', 24, false);
 	setScrollFactor('guy', 1.1, 1.1);
-	scaleObject('guy', 1.4, 1.4)
+	scaleObject('guy', 1.4, 1.4);
 
 
 	makeLuaSprite('stagefront', 'stages/bar/pillars', -600, -400);
 	setScrollFactor('stagefront', 0.95, 0.95);
-	scaleObject('stagefront', 1.4, 1.4)
+	scaleObject('stagefront', 1.4, 1.4);
 
 	addLuaSprite('stageback', false);
 	addLuaSprite('bartender', false);
@@ -32,4 +32,13 @@ function onCreate()
 	addLuaSprite('lad', true);
 	addLuaSprite('other', true);
 	addLuaSprite('guy', true);
+end
+
+function onBeatHit()
+	if curBeat % 2 == 1 or curBeat % 2 == 2 then
+		objectPlayAnimation ('bartender', 'Bartender', false);
+		objectPlayAnimation ('lad', 'HeadAuxBop', false)
+		objectPlayAnimation ('other', 'LotazBop', false)
+		objectPlayAnimation ('guy', 'TWTBop', false)
+	end
 end
